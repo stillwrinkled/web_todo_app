c49474a92f528a0e5f23722e5f0bdbc8cd2169c5
amit_1 <amit@stillwrinkled.com>
2023-05-04 20:17:06 +0530

    implement "Add new todo items"

--- web.py
+++ web.py
@@ -4,16 +4,26 @@ from set_working_directory import set_working_directory
 
 set_working_directory()
 
+todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + '\n')
+    functions.write_todos(todos)
+    print(todo)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity."
          )
-todos = functions.get_todos()
+
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add new todo...")
+st.text_input(label="Enter a todo:", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 print("Executed")
+
+st.session_state
